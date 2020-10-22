e0e9323a3d8574323d1f90e5dfe39ffec7b9e344
MaxJGruber <maxjgruber@gmail.com>
2020-10-22 17:21:20 +0200

    commit before heroku deploy

--- models/Item.js
+++ models/Item.js
@@ -1,48 +1,53 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const itemSchema = new Schema({
-  name: { type: String, required: true },
-  brand: { type: String, required: true },
-  type: {
-    type: String,
-    enum: [
-      "blonde",
-      "stout",
-      "pale ale",
-      "ipa",
-      "cider",
-      "wheat beer",
-      "other",
-    ],
-    required: true,
-  },
-  rating: { type: Number, min: 0, max: 5, required: true },
-  isCraft: Boolean,
-  price: Number,
-  priceHH: Number,
-  description: String,
-  image: {
-    type: String,
-    default:
-      "https://www.sciencenews.org/wp-content/uploads/2020/05/050620_mt_beer_feat-1028x579.jpg",
-  },
-  location: {
+const itemSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    brand: { type: String, required: true },
     type: {
       type: String,
-      enum: ["Point"],
+      enum: [
+        "blonde",
+        "stout",
+        "pale ale",
+        "ipa",
+        "cider",
+        "wheat beer",
+        "other",
+      ],
+      required: true,
     },
-    coordinates: {
-      type: [Number],
+    rating: { type: Number, min: 0, max: 5, required: true },
+    isCraft: Boolean,
+    price: Number,
+    priceHH: Number,
+    description: String,
+    image: {
+      type: String,
+      default:
+        "https://www.sciencenews.org/wp-content/uploads/2020/05/050620_mt_beer_feat-1028x579.jpg",
     },
-    formattedAddress: String,
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+      },
+      coordinates: {
+        type: [Number],
+      },
+      formattedAddress: String,
+    },
+    alcohol_level: Number,
+    added_by: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  alcohol_level: Number,
-  added_by: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  {
+    timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" },
+  }
+);
 
 const Item = mongoose.model("Item", itemSchema);
 
